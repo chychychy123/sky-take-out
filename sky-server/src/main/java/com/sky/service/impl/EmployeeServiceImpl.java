7992a6b4f1c40d5607ec6d11531f343785065205
“chy” <1907576915@qq.com>
2025-04-24 15:20:16 +0800

    公共字段代码填充模块开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -80,11 +80,11 @@ public class EmployeeServiceImpl implements EmployeeService {
     //设置密码，默认密码123456
     employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
     //设置创建时间、修改时间
-    employee.setCreateTime(LocalDateTime.now());
-    employee.setUpdateTime(LocalDateTime.now());
-    //设置当前记录创建人的id和修改人的id
-    employee.setCreateUser(BaseContext.getCurrentId());
-    employee.setUpdateUser(BaseContext.getCurrentId());
+//    employee.setCreateTime(LocalDateTime.now());
+//    employee.setUpdateTime(LocalDateTime.now());
+//    //设置当前记录创建人的id和修改人的id
+//    employee.setCreateUser(BaseContext.getCurrentId());
+//    employee.setUpdateUser(BaseContext.getCurrentId());
 
     employeeMapper.insert(employee);
     }
@@ -117,8 +117,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     public void update(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO,employee);
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+//        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setUpdateUser(BaseContext.getCurrentId());
         employeeMapper.update(employee);
     }
 }

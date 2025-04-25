c75572452d592bec3c58e96a6b0aac4387124d9c
“chy” <1907576915@qq.com>
2025-04-25 15:13:48 +0800

    修改菜品代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -3,6 +3,7 @@ package com.sky.service;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 
 import java.util.List;
 
@@ -13,4 +14,8 @@ public interface DishService {
     PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
     //菜品批量删除
     void deleteBatch(List<Long> ids);
+    //根据id查询菜品和对应口味数据
+    DishVO getByIdWithFlavor(Long id);
+    //根据id修改菜品基本信息和口味信息
+    void updateWithFlavor(DishDTO dishDTO);
 }
